322f245fc703e5203a66cd506d27b39f6f3e8829
pramayu <gedepramayu@gmail.com>
2017-03-09 10:57:22 +0700

    Webpack & express config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import webpack from 'webpack';
+
+
+export default {
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/index.js')
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'javascripts/bundle.js'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
